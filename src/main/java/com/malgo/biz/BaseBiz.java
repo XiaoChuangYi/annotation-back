cdd377e840ee919cad329c9017ae57a9f21cb676
Jack Feng <jackfengji@gmail.com>
2018-06-13 14:08:32 +0800

    feat: add roleId and userId to doBiz function in basebiz

--- src/main/java/com/malgo/biz/BaseBiz.java
+++ src/main/java/com/malgo/biz/BaseBiz.java
@@ -3,29 +3,36 @@ package com.malgo.biz;
 import com.malgo.exception.BusinessRuleException;
 import com.malgo.exception.InvalidInputException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.NotImplementedException;
 
-/**
- * Created by cjl on 2018/5/30.
- */
+/** Created by cjl on 2018/5/30. */
 @Slf4j
-public abstract class BaseBiz<REQ,RES> {
+public abstract class BaseBiz<REQ, RES> {
 
   protected abstract void validateRequest(REQ req) throws InvalidInputException;
-  protected abstract void authorize(int userId,int role,REQ req) throws BusinessRuleException;
-  protected abstract RES doBiz(REQ req);
+
+  protected abstract void authorize(int userId, int role, REQ req) throws BusinessRuleException;
+
+  protected RES doBiz(int userId, int role, REQ req) {
+    return this.doBiz(req);
+  }
+
+  protected RES doBiz(REQ req) {
+    throw new NotImplementedException("do biz is not implemented in " + this.getClass());
+  }
 
   private boolean isServerSideException(final Throwable ex) {
     return !(ex instanceof InvalidInputException) && !(ex instanceof BusinessRuleException);
   }
 
-  public RES process(REQ req, int userId,int role) {
+  public RES process(REQ req, int userId, int role) {
     // TODO add metrics
     log.info("start biz {}, {}", this.getClass(), req);
 
     try {
       validateRequest(req);
-      authorize(userId,role ,req);
-      return doBiz(req);
+      authorize(userId, role, req);
+      return doBiz(userId, role, req);
     } catch (Throwable ex) {
       if (isServerSideException(ex)) {
         log.error(ex.getMessage(), ex);
@@ -38,5 +45,4 @@ public abstract class BaseBiz<REQ,RES> {
       // TODO record metric time
     }
   }
-
 }
