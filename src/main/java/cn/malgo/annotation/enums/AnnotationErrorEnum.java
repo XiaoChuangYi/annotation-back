5ce20c1ac4d62fc91f7ca5680509a820b29970c2
Jack Feng <feng.ji@malgo.cn>
2018-07-10 09:29:28 +0800

    feat: add ENTITY_CONSISTENCY, WRONG_RELATION_TYPE error enum

--- src/main/java/cn/malgo/annotation/enums/AnnotationErrorEnum.java
+++ src/main/java/cn/malgo/annotation/enums/AnnotationErrorEnum.java
@@ -8,7 +8,11 @@ public enum AnnotationErrorEnum {
   // 关联：同一个实体不同类型
   ENTITY_MULTIPLE_TYPE(AnnotationTypeEnum.relation),
   // 关联：孤立实体
-  ISOLATED_ENTITY(AnnotationTypeEnum.relation);
+  ISOLATED_ENTITY(AnnotationTypeEnum.relation),
+  // 关联：实体一致性
+  ENTITY_CONSISTENCY(AnnotationTypeEnum.relation),
+  // 关联：错误关联类型，source->target的关联type有误
+  WRONG_RELATION_TYPE(AnnotationTypeEnum.relation);
 
   @Getter private AnnotationTypeEnum annotationType;
 
