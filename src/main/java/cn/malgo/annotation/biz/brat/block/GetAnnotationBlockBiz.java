ba01e79bb4579cd7616e26c13552b64e8b14a1c9
chenjianlong <17091803alyxCJL@>
2018-07-11 12:19:33 +0800

    feat 添加获取block标注的功能

---
+++ src/main/java/cn/malgo/annotation/biz/brat/block/GetAnnotationBlockBiz.java
@@ -0,0 +1,54 @@
+package cn.malgo.annotation.biz.brat.block;
+
+import cn.malgo.annotation.annotation.RequireRole;
+import cn.malgo.annotation.biz.base.BaseBiz;
+import cn.malgo.annotation.dao.AnnotationTaskBlockRepository;
+import cn.malgo.annotation.entity.AnnotationTaskBlock;
+import cn.malgo.annotation.enums.AnnotationRoleStateEnum;
+import cn.malgo.annotation.enums.AnnotationTaskState;
+import cn.malgo.annotation.exception.InvalidInputException;
+import cn.malgo.annotation.request.brat.GetAutoAnnotationRequest;
+import cn.malgo.annotation.utils.AnnotationConvert;
+import cn.malgo.annotation.vo.AnnotationBlockBratVO;
+import java.util.Optional;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequireRole(AnnotationRoleStateEnum.admin)
+public class GetAnnotationBlockBiz
+    extends BaseBiz<GetAutoAnnotationRequest, AnnotationBlockBratVO> {
+
+  private final AnnotationTaskBlockRepository annotationTaskBlockRepository;
+
+  public GetAnnotationBlockBiz(AnnotationTaskBlockRepository annotationTaskBlockRepository) {
+    this.annotationTaskBlockRepository = annotationTaskBlockRepository;
+  }
+
+  @Override
+  protected void validateRequest(GetAutoAnnotationRequest baseAnnotationRequest)
+      throws InvalidInputException {
+    if (baseAnnotationRequest.getId() <= 0) {
+      throw new InvalidInputException("invalid-id", "无效的id");
+    }
+  }
+
+  @Override
+  protected AnnotationBlockBratVO doBiz(
+      int userId, int role, GetAutoAnnotationRequest baseAnnotationRequest) {
+    Optional<AnnotationTaskBlock> optional =
+        annotationTaskBlockRepository.findById(baseAnnotationRequest.getId());
+    if (optional.isPresent()) {
+      final AnnotationTaskBlock annotationTaskBlock = optional.get();
+      if (StringUtils.equalsAny(
+          annotationTaskBlock.getState().name(),
+          AnnotationTaskState.ANNOTATED.name(),
+          AnnotationTaskState.FINISHED.name())) {
+        return AnnotationConvert.convert2AnnotationBlockBratVO(annotationTaskBlock);
+      } else {
+        throw new InvalidInputException("no-permission-handle-current-record", "无权操作当前状态的block");
+      }
+    }
+    return null;
+  }
+}
