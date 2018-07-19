26492094d4f32e4879060b31f1983e320bfff9e4
Jack Feng <feng.ji@malgo.cn>
2018-07-19 15:17:29 +0800

    feat: remove exercise related code

--- src/main/java/cn/malgo/annotation/dto/AnnotationExercise.java
+++
@@ -1,8 +0,0 @@
-package cn.malgo.annotation.dto;
-
-/** Created by cjl on 2018/6/4. */
-public interface AnnotationExercise {
-  int getAnnotationId();
-
-  long getNum();
-}
--- src/main/java/cn/malgo/annotation/request/exercise/UserResetRequest.java
+++
@@ -1,9 +0,0 @@
-package cn.malgo.annotation.request.exercise;
-
-import lombok.Data;
-
-/** Created by cjl on 2018/6/4. */
-@Data
-public class UserResetRequest {
-  private int userId;
-}
