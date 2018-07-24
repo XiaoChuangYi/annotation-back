2dba6bf82b0b851bca551cf535ef67997725cc82
chenjianlong <17091803alyxCJL@>
2018-07-24 11:14:09 +0800

    refact 重构任务detail查询

--- src/main/java/cn/malgo/annotation/biz/task/ListTaskDetailsBiz.java
+++ src/main/java/cn/malgo/annotation/biz/task/ListTaskDetailsBiz.java
@@ -1,11 +1,9 @@
 package cn.malgo.annotation.biz.task;
 
 import cn.malgo.annotation.constants.Permissions;
-import cn.malgo.annotation.dao.AnnotationTaskDocRepository;
 import cn.malgo.annotation.dao.AnnotationTaskRepository;
 import cn.malgo.annotation.dao.OriginalDocRepository;
 import cn.malgo.annotation.entity.AnnotationTask;
-import cn.malgo.annotation.entity.AnnotationTaskDoc;
 import cn.malgo.annotation.request.task.ListTaskDetailRequest;
 import cn.malgo.annotation.vo.AnnotationTaskDetailVO;
 import cn.malgo.annotation.vo.OriginalDocVO;
@@ -15,22 +13,19 @@ import cn.malgo.service.exception.InvalidInputException;
 import cn.malgo.service.model.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
 @RequirePermission(Permissions.ADMIN)
 public class ListTaskDetailsBiz extends BaseBiz<ListTaskDetailRequest, AnnotationTaskDetailVO> {
-  private final AnnotationTaskDocRepository annotationTaskDocRepository;
+
   private final OriginalDocRepository originalDocRepository;
   private final AnnotationTaskRepository annotationTaskRepository;
 
   public ListTaskDetailsBiz(
-      AnnotationTaskDocRepository annotationTaskDocRepository,
       OriginalDocRepository originalDocRepository,
       AnnotationTaskRepository annotationTaskRepository) {
-    this.annotationTaskDocRepository = annotationTaskDocRepository;
     this.originalDocRepository = originalDocRepository;
     this.annotationTaskRepository = annotationTaskRepository;
   }
@@ -45,32 +40,22 @@ public class ListTaskDetailsBiz extends BaseBiz<ListTaskDetailRequest, Annotatio
 
   @Override
   protected AnnotationTaskDetailVO doBiz(ListTaskDetailRequest request, UserDetails user) {
-    List<OriginalDocVO> originalDocVOList = new ArrayList<>();
-    // task任务还没有文档详情的情况
     final AnnotationTask annotationTask = annotationTaskRepository.getOne(request.getId());
-    final List<AnnotationTaskDoc> annotationTaskDocList =
-        annotationTaskDocRepository.findByTask_Id(request.getId());
-
-    if (annotationTaskDocList.size() > 0) {
-      final List<Long> idList =
-          annotationTaskDocList.stream().map(x -> x.getDoc().getId()).collect(Collectors.toList());
-      originalDocVOList =
-          originalDocRepository
-              .findAllById(idList)
-              .stream()
-              .map(x -> new OriginalDocVO(x.getId(), x.getType(), x.getState().name()))
-              .collect(Collectors.toList());
-    }
+    final List<OriginalDocVO> originalDocVOList =
+        originalDocRepository
+            .findByTasks_Task_IdEquals(request.getId())
+            .stream()
+            .map(
+                originalDoc ->
+                    new OriginalDocVO(
+                        originalDoc.getId(), originalDoc.getType(), originalDoc.getState().name()))
+            .collect(Collectors.toList());
 
     return new AnnotationTaskDetailVO(
         annotationTask.getName(),
         annotationTask.getState().name(),
         originalDocVOList,
         originalDocVOList.size(),
-        annotationTask
-            .getTaskDocs()
-            .stream()
-            .mapToLong(annotationTaskDoc -> annotationTaskDoc.getBlocks().size())
-            .sum());
+        annotationTask.getTotalBranchNum());
   }
 }
--- src/main/java/cn/malgo/annotation/dao/OriginalDocRepository.java
+++ src/main/java/cn/malgo/annotation/dao/OriginalDocRepository.java
@@ -1,8 +1,12 @@
 package cn.malgo.annotation.dao;
 
 import cn.malgo.annotation.entity.OriginalDoc;
+import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface OriginalDocRepository
-    extends JpaRepository<OriginalDoc, Long>, JpaSpecificationExecutor<OriginalDoc> {}
+    extends JpaRepository<OriginalDoc, Long>, JpaSpecificationExecutor<OriginalDoc> {
+
+  Set<OriginalDoc> findByTasks_Task_IdEquals(long taskId);
+}
