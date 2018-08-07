02b0a139aa9fa0aac8a11d3e10889d866f80dd29
Jack Feng <feng.ji@malgo.cn>
2018-08-07 19:44:17 +0800

    fix: 修复重新登录后用户不修改的问题
    SessionAttribute会将Model的数据再放回sesssion去，导致session永远更新不成功

--- src/main/java/cn/malgo/annotation/controller/UserAccountController.java
+++ src/main/java/cn/malgo/annotation/controller/UserAccountController.java
@@ -4,15 +4,18 @@ import cn.malgo.annotation.biz.AddUserAccountBiz;
 import cn.malgo.annotation.biz.ListUserAccountBiz;
 import cn.malgo.annotation.biz.ModifyUserPasswordBiz;
 import cn.malgo.annotation.biz.SetUserStateBiz;
-import cn.malgo.annotation.request.*;
-import cn.malgo.annotation.service.UserAccountService;
+import cn.malgo.annotation.request.AddUserAccountRequest;
+import cn.malgo.annotation.request.ListUserAccountRequest;
+import cn.malgo.annotation.request.ModifyPasswordRequest;
+import cn.malgo.annotation.request.SetUserStateRequest;
 import cn.malgo.service.model.Response;
 import cn.malgo.service.model.UserDetails;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/v2")
@@ -20,46 +23,18 @@ import javax.servlet.http.HttpServletResponse;
 public class UserAccountController extends BaseController {
   private final ListUserAccountBiz listUserAccountBiz;
   private final AddUserAccountBiz addUserAccountBiz;
-  private final UserAccountService userAccountService;
   private final ModifyUserPasswordBiz modifyUserPasswordBiz;
   private final SetUserStateBiz setUserStateBiz;
 
   public UserAccountController(
       final ListUserAccountBiz listUserAccountBiz,
       final AddUserAccountBiz addUserAccountBiz,
-      final UserAccountService userAccountService,
       final ModifyUserPasswordBiz modifyUserPasswordBiz,
       final SetUserStateBiz setUserStateBiz) {
     this.listUserAccountBiz = listUserAccountBiz;
     this.addUserAccountBiz = addUserAccountBiz;
-    this.userAccountService = userAccountService;
     this.modifyUserPasswordBiz = modifyUserPasswordBiz;
     this.setUserStateBiz = setUserStateBiz;
-  }
-
-  /** 用户登录 */
-  @RequestMapping(value = "/user/login", method = RequestMethod.POST)
-  public Response login(
-      @RequestBody LoginRequest loginRequest,
-      HttpServletRequest servletRequest,
-      HttpServletResponse servletResponse) {
-    return new Response<>(userAccountService.login(loginRequest, servletRequest, servletResponse));
-  }
-
-  /** 用户登出 */
-  @RequestMapping(value = "/log-out", method = RequestMethod.POST)
-  public Response logout(
-      @RequestBody LogOutRequest logOutRequest,
-      HttpServletRequest servletRequest,
-      HttpServletResponse servletResponse) {
-    return new Response<>(
-        userAccountService.logOut(logOutRequest, servletRequest, servletResponse));
-  }
-
-  @RequestMapping(value = "/log-refresh", method = RequestMethod.GET)
-  public Response logRefresh(
-      HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
-    return new Response<>(userAccountService.loginRefresh(servletRequest, servletResponse));
   }
 
   /** 用户列表查询 */
