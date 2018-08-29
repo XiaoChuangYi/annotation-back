244545d8974eda5977722ea8dd05f63be6e486e6
chenjianlong <17091803alyxCJL@>
2018-08-29 20:11:17 +0800

    [feat] 批次、人员正确率汇总计算优化（排除那些放弃标注的

--- src/main/java/cn/malgo/annotation/biz/CleanOutBlockBiz.java
+++ src/main/java/cn/malgo/annotation/biz/CleanOutBlockBiz.java
@@ -16,6 +16,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
