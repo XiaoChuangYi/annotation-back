c19f3fbfcc29fa0cf54fef0a684f4ba1433b775d
Jack Feng <feng.ji@malgo.cn>
2018-08-29 18:16:54 +0800

    chore: change commit annotations script to add total

--- scripts/commit_annotations.py
+++ scripts/commit_annotations.py
@@ -7,11 +7,14 @@ if __name__ == '__main__':
     session = login(url, 'test1', '123456')
     page_size = 20
     count = 0
+    total = 0
 
     while True:
         res = session.get(f'{url}/api/v2/list-annotation?pageIndex=1&pageSize={page_size}&annotationTypes=2&states=PRE_ANNOTATION&states=ANNOTATION_PROCESSING').json()['data']
         if res['total'] == 0:
             break
+        if not total:
+            total = res['total']
 
         for annotation in res['dataList']:
             session.post(f'{url}/api/v2/commit-annotation', json={
@@ -20,4 +23,4 @@ if __name__ == '__main__':
             })
             count += 1
 
-        print(f'committed {count} annotations')
+        print(f'committed {count}/{total} annotations')
