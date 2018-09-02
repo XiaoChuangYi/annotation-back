4075c6d501578cef9cf7713d2ff9a1e1ef3478d7
Jack Feng <feng.ji@malgo.cn>
2018-09-02 23:02:04 +0800

    feat: try to use docker memory limit instead of Xmx to set jvm memory

--- APP-META/start.sh
+++ APP-META/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eo pipefail
 
-/usr/bin/java ${JAVA_OPTS} -Xmx${MAX_MEMORY} -jar /usr/src/app/${APP_NAME}.jar --spring.profiles.active=${STAGE}
+/usr/bin/java ${JAVA_OPTS} -jar /usr/src/app/${APP_NAME}.jar --spring.profiles.active=${STAGE}
