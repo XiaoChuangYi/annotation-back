de8c40d38f3a75019a2f2ab23c74ce369d58056c
chenjianlong <17091803alyxCJL@>
2018-09-04 12:51:06 +0800

    [feat] 新增用户中心相关内容

--- src/main/java/cn/malgo/annotation/config/AuthConfig.java
+++ src/main/java/cn/malgo/annotation/config/AuthConfig.java
@@ -1,6 +1,7 @@
 package cn.malgo.annotation.config;
 
-import cn.malgo.annotation.interceptor.LoginInterceptor;
+import cn.malgo.common.auth.interceptor.LoginInterceptor;
+import cn.malgo.common.auth.interceptor.PermissionInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,14 @@ public class AuthConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     registry
         .addInterceptor(new LoginInterceptor())
+        .addPathPatterns("/**")
+        .excludePathPatterns("/api/v2/user/login")
+        .excludePathPatterns("/api/v2/import")
+        .excludePathPatterns("/api/v2/doc/import")
+        .excludePathPatterns("/api/v2/list-type")
+        .excludePathPatterns("/static/*");
+    registry
+        .addInterceptor(new PermissionInterceptor())
         .addPathPatterns("/**")
         .excludePathPatterns("/api/v2/user/login")
         .excludePathPatterns("/api/v2/import")
