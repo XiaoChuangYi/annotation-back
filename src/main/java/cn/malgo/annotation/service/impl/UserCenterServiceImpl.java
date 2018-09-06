482b82d1cdf67feb39e667207f262e8a5279adf5
chenjianlong <17091803alyxCJL@>
2018-09-06 09:51:56 +0800

    [feat] 添加测试类配置文件

--- src/main/java/cn/malgo/annotation/service/impl/UserCenterServiceImpl.java
+++ src/main/java/cn/malgo/annotation/service/impl/UserCenterServiceImpl.java
@@ -4,11 +4,9 @@ import cn.malgo.annotation.dto.User;
 import cn.malgo.annotation.service.AuthService;
 import cn.malgo.annotation.service.UserCenterService;
 import cn.malgo.annotation.service.feigns.UserCenterClient;
-import cn.malgo.annotation.vo.UserInfo;
 import cn.malgo.common.auth.AuthConstants;
 import cn.malgo.common.auth.RedisConfigService;
 import cn.malgo.service.exception.BusinessRuleException;
-import com.alibaba.fastjson.JSONObject;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -30,10 +28,14 @@ public class UserCenterServiceImpl implements UserCenterService {
 
   @Override
   public List<User> getUsersByUserCenter() {
-    if (authService.login()) {
+    if (redisConfigService.exists(AuthConstants.ALL_SYSTEM_TICKET)) {
       return userCenterClient.getUsers().getUsers();
     } else {
-      throw new BusinessRuleException("", "用户中心登陆失败，无法获取用户信息！");
+      if (authService.login()) {
+        return userCenterClient.getUsers().getUsers();
+      } else {
+        throw new BusinessRuleException("", "用户中心登陆失败，无法获取用户信息！");
+      }
     }
   }
 }
