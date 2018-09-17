469aafd6b206b7050f9f2d10d7e600b3d27a45a6
chenjianlong <17091803alyxCJL@>
2018-09-17 13:26:36 +0800

    [feat]用户接口过滤

---
+++ src/main/java/cn/malgo/annotation/request/GetUsersRequest.java
@@ -0,0 +1,12 @@
+package cn.malgo.annotation.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetUsersRequest {
+  private String rank;
+}
--- src/main/java/cn/malgo/annotation/service/feigns/UserCenterClient.java
+++ src/main/java/cn/malgo/annotation/service/feigns/UserCenterClient.java
@@ -1,8 +1,10 @@
 package cn.malgo.annotation.service.feigns;
 
+import cn.malgo.annotation.request.GetUsersRequest;
 import cn.malgo.annotation.vo.UserInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -14,5 +16,5 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface UserCenterClient {
 
   @RequestMapping(method = RequestMethod.POST, value = "/api/user/get-users")
-  UserInfo getUsers();
+  UserInfo getUsers(@RequestBody GetUsersRequest request);
 }
