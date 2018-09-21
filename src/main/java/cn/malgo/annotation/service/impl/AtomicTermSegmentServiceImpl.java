11875e73725e2114d65f15b81029e1218b8a260c
Jack Feng <feng.ji@malgo.cn>
2018-09-21 14:54:21 +0800

    feat: use IKAnalyzer to segment disease attributes

--- src/main/java/cn/malgo/annotation/service/impl/AtomicTermSegmentServiceImpl.java
+++ src/main/java/cn/malgo/annotation/service/impl/AtomicTermSegmentServiceImpl.java
@@ -5,21 +5,26 @@ import cn.malgo.annotation.entity.AtomicTerm;
 import cn.malgo.annotation.enums.AnnotationTypeEnum;
 import cn.malgo.annotation.service.AtomicTermSegmentService;
 import cn.malgo.core.definition.Entity;
-import com.hankcs.hanlp.collection.trie.bintrie.BinTrie;
-import com.hankcs.hanlp.corpus.tag.Nature;
-import com.hankcs.hanlp.dictionary.CoreDictionary.Attribute;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.springframework.stereotype.Service;
+import org.wltea.analyzer.cfg.DefaultConfig;
+import org.wltea.analyzer.dic.Dictionary;
+import org.wltea.analyzer.lucene.IKAnalyzer;
 
 @Service
 @Slf4j
 public class AtomicTermSegmentServiceImpl implements AtomicTermSegmentService {
   private final AtomicTermRepository atomicTermRepository;
-  private HashMap<AnnotationTypeEnum, BinTrie<Attribute>> tries = new HashMap<>();
+  private final HashMap<String, String> wordTypes = new HashMap<>(10000);
+  private IKAnalyzer analyzer;
 
   public AtomicTermSegmentServiceImpl(final AtomicTermRepository atomicTermRepository) {
     this.atomicTermRepository = atomicTermRepository;
@@ -27,50 +32,64 @@ public class AtomicTermSegmentServiceImpl implements AtomicTermSegmentService {
 
   @PostConstruct
   public void init() {
+    final List<String> words = new ArrayList<>();
+
     for (final AtomicTerm atomicTerm : atomicTermRepository.findAll()) {
-      tries
-          .computeIfAbsent(atomicTerm.getAnnotationType(), (ann) -> new BinTrie<>())
-          .put(atomicTerm.getTerm(), new Attribute(Nature.create(atomicTerm.getAnType()), 1));
+      if (atomicTerm.getAnnotationType() == AnnotationTypeEnum.disease) {
+        words.add(atomicTerm.getTerm());
+        wordTypes.put(atomicTerm.getTerm(), atomicTerm.getAnType());
+      }
     }
 
-    tries.forEach(
-        (key, value) -> {
-          log.info("atomic term loaded: {}, size: {}", key, value.size());
-        });
+    Dictionary.initial(DefaultConfig.getInstance());
+    Dictionary dict = Dictionary.getSingleton();
+    dict.addWords(words);
+
+    this.analyzer = new IKAnalyzer(true);
   }
 
   @Override
   public List<Entity> seg(final AnnotationTypeEnum annotationType, final String text) {
-    if (tries.containsKey(annotationType)) {
-      final List<Entity> entities = new ArrayList<>();
-      tries
-          .get(annotationType)
-          .parseText(
-              text,
-              (begin, end, value) -> {
-                if (value != null && value.nature.length > 0) {
-                  entities.add(
-                      new Entity(
-                          "T" + (entities.size() + 1),
-                          begin,
-                          end,
-                          value.nature[0].toString(),
-                          text.substring(begin, end)));
-                }
-              });
-      return entities;
+    if (annotationType != AnnotationTypeEnum.disease) {
+      log.warn("unsupported annotation type {}", annotationType);
+      return new ArrayList<>();
     }
 
-    return null;
+    try {
+      final List<Entity> result = new ArrayList<>();
+
+      final TokenStream ts = analyzer.tokenStream("title", text);
+      final CharTermAttribute cta = ts.addAttribute(CharTermAttribute.class);
+      ts.reset();
+
+      int i = 0, index = 0;
+      while (ts.incrementToken()) {
+        i++;
+
+        final String term = cta.toString();
+        final int start = text.indexOf(term, index);
+
+        if (wordTypes.containsKey(term)) {
+          result.add(new Entity("T" + i, start, start + term.length(), term, wordTypes.get(term)));
+        }
+
+        index = start + term.length();
+      }
+
+      return result;
+    } catch (IOException e) {
+      log.warn("segment failed", e);
+      return new ArrayList<>();
+    }
   }
 
   @Override
   public void addAtomicTerms(
       final AnnotationTypeEnum annotationType, final List<AtomicTerm> newTerms) {
-    final BinTrie<Attribute> trie = tries.computeIfAbsent(annotationType, (ann) -> new BinTrie<>());
-    newTerms.forEach(
-        atomicTerm ->
-            trie.put(
-                atomicTerm.getTerm(), new Attribute(Nature.create(atomicTerm.getAnType()), 1)));
+    if (annotationType == AnnotationTypeEnum.disease) {
+      Dictionary.getSingleton()
+          .addWords(newTerms.stream().map(AtomicTerm::getTerm).collect(Collectors.toList()));
+      newTerms.forEach(term -> wordTypes.put(term.getTerm(), term.getAnType()));
+    }
   }
 }
