ca849fd103832562d306963c106f363edc437714
chenjianlong <17091803alyxCJL@>
2019-01-30 16:35:12 +0800

    [feat] 修改配置文件

--- APP-META/start.sh
+++ APP-META/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eo pipefail
 
-/usr/bin/java ${JAVA_OPTS} -XX:NewRatio=${MEMORY_NEW_RATIO} -Xms${MAN_MEMORY} -Xmx${MAX_MEMORY} -jar /usr/src/app/${APP_NAME}.jar --spring.profiles.active=${STAGE}
+/usr/bin/java ${JAVA_OPTS} -XX:NewRatio=${MEMORY_NEW_RATIO} -Xms${MAX_MEMORY} -Xmx${MAX_MEMORY} -jar /usr/src/app/${APP_NAME}.jar --spring.profiles.active=${STAGE}
